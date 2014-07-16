a0776e71b16d4e39cdde7b2a515400e20c7310e4
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-16 15:24:45 -0600

    maybe migrate

--- db/migrate/1405544328_fuck.rb
+++ db/migrate/1405544328_fuck.rb
@@ -1,16 +1,15 @@
 class Fuck < ActiveRecord::Migration
   def up
-    def up
-      create_table :favorites do |t|
-        t.integer :users_id
-        t.integer :fish_id
-      end
-    end
-
-    def down
-      drop_table :users
-      drop_table :fish
-      drop_table :favorites
+    create_table :favorites do |t|
+      t.integer :users_id
+      t.integer :fish_id
     end
   end
+
+  def down
+    drop_table :users
+    drop_table :fish
+    drop_table :favorites
+  end
 end
+
