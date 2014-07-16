9f64722a8f208e9ffe81d0d3ad6a42f7f3815ce2
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-16 15:32:11 -0600

    praying

---
+++ db/migrate/1405546281_snakecase.rb
@@ -0,0 +1,23 @@
+class Snakecase < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+    end
+    create_table :fish do |t|
+      t.string :users_id
+      t.string :name
+      t.string :wiki
+    end
+    create_table :favorites do |t|
+      t.integer :users_id
+      t.integer :fish_id
+    end
+  end
+
+  def down
+    drop_table :users
+    drop_table :fish
+    drop_table :favorites
+  end
+end
