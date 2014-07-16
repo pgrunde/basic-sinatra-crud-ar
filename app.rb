c42fd503fd3e835cfc6370134ce24f6218d082ae
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-16 14:10:02 -0600

    delete function works maybe

--- app.rb
+++ app.rb
@@ -48,64 +48,72 @@ class App < Sinatra::Application
     end
   end
 
-  get "/" do
-    # if session[:user_id]
-    #   @username = @database_connection.sql("SELECT username FROM users WHERE id=#{session[:user_id]};").first["username"]
-    #   @user_arr = @database_connection.sql("SELECT username FROM users;").map {|hash| hash["username"] if hash["username"] != @username}
-    #   @user_arr.delete(nil)
-    #
-    #   @fav_fish_arr = @database_connection.sql(
-    #     "SELECT favorites.id, fish.name as fish_name FROM favorites " +
-    #     "INNER JOIN users on users.id = favorites.users_id " +
-    #     "INNER JOIN fish on fish.id = favorites.fish_id " +
-    #     "WHERE users.id = '#{session[:user_id]}'"
-    #     ).uniq
-    #
-    #   @fish_arr = @database_connection.sql("SELECT name, wiki FROM fish WHERE users_id = '#{session[:user_id]}';")
-    # end
+  # get "/" do
+  #   if session[:user_id]
+  #     @username = @database_connection.sql("SELECT username FROM users WHERE id=#{session[:user_id]};").first["username"]
+  #     @user_arr = @database_connection.sql("SELECT username FROM users;").map {|hash| hash["username"] if hash["username"] != @username}
+  #     @user_arr.delete(nil)
+  #
+  #     @fav_fish_arr = @database_connection.sql(
+  #       "SELECT favorites.id, fish.name as fish_name FROM favorites " +
+  #       "INNER JOIN users on users.id = favorites.users_id " +
+  #       "INNER JOIN fish on fish.id = favorites.fish_id " +
+  #       "WHERE users.id = '#{session[:user_id]}'"
+  #       ).uniq
+  #
+  #     @fish_arr = @database_connection.sql("SELECT name, wiki FROM fish WHERE users_id = '#{session[:user_id]}';")
+  #   end
+  #
+  #   if params[:sort] == "asc"
+  #     @user_arr.sort!
+  #   elsif params[:sort] == "desc"
+  #     @user_arr.sort! { |x,y| y <=> x }
+  #   end
+  #   if session[:clicked_user_id]
+  #     @click_user_fish = @database_connection.sql("SELECT id, name, wiki FROM fish WHERE users_id = '#{session[:clicked_user_id]}';")
+  #   else
+  #     @click_user_fish = []
+  #   end
+  #
+  #   erb :root, :locals => {:username => @username,
+  #                          :user_arr => @user_arr,
+  #                          :fish_arr => @fish_arr,
+  #                          :click_user_fish => @click_user_fish,
+  #                          :fav_fish_arr => @fav_fish_arr}, :layout => :main_layout
+  # end
 
-    if params[:sort] == "asc"
-      @user_arr.sort!
-    elsif params[:sort] == "desc"
-      @user_arr.sort! { |x,y| y <=> x }
-    end
-    if session[:clicked_user_id]
-      @click_user_fish = @database_connection.sql("SELECT id, name, wiki FROM fish WHERE users_id = '#{session[:clicked_user_id]}';")
-    else
-      @click_user_fish = []
-    end
-
-    erb :root, :locals => {:username => @username,
-                           :user_arr => @user_arr,
-                           :fish_arr => @fish_arr,
-                           :click_user_fish => @click_user_fish,
-                           :fav_fish_arr => @fav_fish_arr}, :layout => :main_layout
+  get "/fish/" do
+    erb :create_fish, :layout => :main_layout
   end
 
   get "/register/" do
     erb :register, :layout => :main_layout
   end
 
-  post "/delete/:username" do
-    @username = @database_connection.sql("SELECT username FROM users WHERE id=#{session[:user_id]};").first["username"]
-    @user_arr = @database_connection.sql("SELECT username FROM users;").map {|hash| hash["username"] if hash["username"] != @username}
-    @user_arr.delete(nil)
-    to_delete = params[:delete]
-    if to_delete == @username
-      redirect "/"
-    else
-      begin
-        @database_connection.sql("DELETE FROM users WHERE username = '#{to_delete}'")
-        redirect "/"
-      rescue
-        redirect "/"
-      end
-    end
+  delete "/delete/:username" do
+    @database_connection.sql("DELETE FROM users WHERE username = '#{params[:username]}'")
+    redirect "/"
   end
 
   post "/fish/" do
+    unless session[:user_id]
+      redirect "/"
+    end
+
+    if params[:name] == "" && params[:wiki] == ""
+      flash[:notice] = "Please enter a name and wikipedia page."
+      redirect "/fish/"
+    elsif params[:name] == ""
+      flash[:notice] = "Please enter a name."
+      redirect "/fish/"
+    elsif params[:wiki] == ""
+      flash[:notice] = "Please enter a wikipedia page."
+      redirect "/fish/"
+    end
+
     @database_connection.sql("INSERT INTO fish (users_id, name, wiki) VALUES (#{session[:user_id]},'#{params[:name]}','#{params[:wiki]}')")
     redirect "/"
+
   end
 
   post "/register/" do
